e0ff428cef0ebe37bd7977add136a1d232cb8568
Student <example@example.com>
2019-10-30 10:18:37 -0700

    basic file structor for using api keys and displaying json data

--- ConsoleApiCall/Program.cs
+++ ConsoleApiCall/Program.cs
@@ -3,19 +3,28 @@ using System.Threading.Tasks;
 using RestSharp;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
-
-namespace ApiTest
+namespace ConsoleApiCall
 {
   class Program
   {
-    static void Main(string[] args)
+    static void Main()
     {
-      var apiCallTask = ApiHelper.ApiCall("[xQmcdPgSS9nvHvXzjjTG4ACQZbWEjmWm]");
+      var apiCallTask = ApiHelper.ApiCall("[YOUR-API-KEY-HERE]");
       var result = apiCallTask.Result;
-      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
-      Console.WriteLine(jsonResponse["results"]);
 
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      List<Article> articleList = JsonConvert.DeserializeObject<List<Article>>(jsonResponse["results"].ToString());
+
+      foreach (Article article in articleList)
+      {
+        Console.WriteLine($"Section: {article.Section}");
+        Console.WriteLine($"Title: {article.Title}");
+        Console.WriteLine($"Abstract: {article.Abstract}");
+        Console.WriteLine($"Url: {article.Url}");
+        Console.WriteLine($"Byline: {article.Byline}");
+      }
     }
   }
 
@@ -24,7 +33,7 @@ namespace ApiTest
     public static async Task<string> ApiCall(string apiKey)
     {
       RestClient client = new RestClient("https://api.nytimes.com/svc/topstories/v2");
-      RestRequest request = new RestRequest($"home.json?api-key={"xQmcdPgSS9nvHvXzjjTG4ACQZbWEjmWm"}", Method.GET);
+      RestRequest request = new RestRequest($"home.json?api-key={apiKey}", Method.GET);
       var response = await client.ExecuteTaskAsync(request);
       return response.Content;
     }
