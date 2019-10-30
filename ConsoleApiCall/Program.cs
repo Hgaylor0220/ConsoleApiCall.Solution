fd17945ed0e39cd8c1c50409107a101c9ab519f0
Student <example@example.com>
2019-10-30 10:03:22 -0700

    created the initial program.cs structure with Newtonsoft and Restsharp pkg

---
+++ ConsoleApiCall/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace ApiTest
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var apiCallTask = ApiHelper.ApiCall("[xQmcdPgSS9nvHvXzjjTG4ACQZbWEjmWm]");
+      var result = apiCallTask.Result;
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      Console.WriteLine(jsonResponse["results"]);
+
+    }
+  }
+
+  class ApiHelper
+  {
+    public static async Task<string> ApiCall(string apiKey)
+    {
+      RestClient client = new RestClient("https://api.nytimes.com/svc/topstories/v2");
+      RestRequest request = new RestRequest($"home.json?api-key={"xQmcdPgSS9nvHvXzjjTG4ACQZbWEjmWm"}", Method.GET);
+      var response = await client.ExecuteTaskAsync(request);
+      return response.Content;
+    }
+  }
+}
